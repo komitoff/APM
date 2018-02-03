e99cad9dc538c09b007afe9dbdd1de91577019f5
komitoff <todorkomitov@gmail.com>
2018-02-03 14:29:43 +0200

    Wired the filter with the products list

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -11,7 +11,17 @@ export class ProductListComponent {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+  _listFilter: string = 'cart';
+  filteredProducts: IProduct[];
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
   products: IProduct[] = [
     {
       "productId": 1,
@@ -35,7 +45,19 @@ export class ProductListComponent {
     }
   ];
 
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
+  }
+
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product : IProduct) => 
+      product.productName.toLocaleLowerCase().indexOf(filterBy) != -1
+    )
   }
 }
