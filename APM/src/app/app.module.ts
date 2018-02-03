4972b88452220bbdd5f71644b7e0170da18207ff
komitoff <todorkomitov@gmail.com>
2018-02-03 14:44:00 +0200

    Implemented star component html and css

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './pipes/convert-to-spaces.pipe';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 
 @NgModule({
